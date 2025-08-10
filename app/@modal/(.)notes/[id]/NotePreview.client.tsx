55df6aaaa5ebe505f62a462d056887b5ae8e6fe2
Misha Busko <Misha.1996@ukr.net>
2025-08-10 21:53:28 +0300

    fix

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,7 +2,9 @@
 
 import { useRouter } from "next/navigation";
 import Modal from "@/components/Modal/Modal";
-import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
+import css from "./NotePreview.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
 
 type NotePreviewProps = {
   id: string;
@@ -15,9 +17,48 @@ export default function NotePreviewClient({ id }: NotePreviewProps) {
     router.back();
   };
 
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["notes", id],
+    queryFn: () => fetchNoteById(id),
+    enabled: Boolean(id),
+    refetchOnMount: false,
+  });
+
   return (
     <Modal onClose={handleClose}>
-      <NoteDetailsClient id={id} />
+      {isLoading && <p>Loading, please wait...</p>}
+      {isError && <p>Something went wrong.</p>}
+      {!isLoading && !isError && note && (
+        <div className={css.container}>
+          <div className={css.item}>
+            <button onClick={handleClose} className={css.backBtn}>
+              Go Back
+            </button>
+            <div className={css.header}>
+              <h2>{note?.title}</h2>
+            </div>
+            <p className={css.content}>{note.content}</p>
+            {note.tag && (
+              <p className={css.tag}>
+                <strong>Tag:</strong> {note.tag}
+              </p>
+            )}
+            <p className={css.date}>
+              {note.updatedAt === note.createdAt
+                ? `Created at: ${new Date(note.createdAt).toLocaleString(
+                    "uk-UA"
+                  )}`
+                : `Updated at: ${new Date(note.updatedAt).toLocaleString(
+                    "uk-UA"
+                  )}`}
+            </p>
+          </div>
+        </div>
+      )}
     </Modal>
   );
 }
--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -9,6 +9,7 @@ const TAGS = ["All", "Todo", "Work", "Personal", "Meeting", "Shopping"];
 export default function TagsMenu() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
 
   return (
     <div className={css.menuContainer}>
@@ -19,7 +20,11 @@ export default function TagsMenu() {
         <ul className={css.menuList}>
           {TAGS.map((tag) => (
             <li key={tag} className={css.menuItem}>
-              <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+              <Link
+                href={`/notes/filter/${tag}`}
+                className={css.menuLink}
+                onClick={closeMenu}
+              >
                 {tag}
               </Link>
             </li>
